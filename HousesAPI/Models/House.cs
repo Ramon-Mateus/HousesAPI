c20be56c1c85aaa1f62d44ca39b73c78ebc1558d
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-05-21 18:08:46 -0300

    feat: house model

---
+++ HousesAPI/Models/House.cs
@@ -0,0 +1,14 @@
+﻿namespace HousesAPI.Models
+{
+    public class House
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Photo { get; set; } = string.Empty;
+        public int AvaibleUnits { get; set; }
+        public Boolean Wifi {  get; set; }
+        public Boolean Laundry { get; set; }
+    }
+}
