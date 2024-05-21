e8325715dd0a47d522aaacf794d628c601e31f4d
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-05-21 19:02:46 -0300

    feat: house controller, get endpoint and DbSet House in context

--- HousesAPI/Contexts/HouseContext.cs
+++ HousesAPI/Contexts/HouseContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HousesAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousesAPI.Contexts
 {
     public class HouseContext : DbContext
     {
+        public DbSet<House> Houses => Set<House>();
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=houses.sqlite3");
---
+++ HousesAPI/Controllers/HousesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HousesAPI.Contexts;
+using HousesAPI.Models;
+
+namespace HousesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HousesController : ControllerBase
+    {
+        private readonly HouseContext _context;
+
+        public HousesController(HouseContext context) => _context = context;
+
+        [HttpGet]
+        public ActionResult<List<House>> GetAllHouses()
+        {
+            var houses = _context.Houses.ToList();
+            return houses;
+        }
+    }
+}
