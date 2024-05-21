7460786253864324521ba9a65e9122adc7f9ab0e
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-05-21 18:03:46 -0300

    feat: house context

---
+++ HousesAPI/Contexts/HouseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HousesAPI.Contexts
+{
+    public class HouseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=houses.sqlite3");
+        }
+    }
+}
